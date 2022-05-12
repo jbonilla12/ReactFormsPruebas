37225b1519e499225717899666d99a1f8137cc3d
jbonilla12 <joshua.lino12@gmail.com>
2022-05-12 10:57:22 -0600

    Editar componentes

--- src/components/FormBootstrap.jsx
+++ src/components/FormBootstrap.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 const FormBootstrap = () => {
   return (
-    <form>
-      Bootstrap
-    </form>
+    <Form>
+      <h2>Nuevo</h2>
+      <Form.Group className="mb-3">
+        <Form.Select aria-label="Default select example">
+          <option>Open this select menu</option>
+          <option value="1">Liga Española</option>
+          <option value="2">Liga Italiana</option>
+          <option value="3">Liga Alemana</option>
+        </Form.Select>
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Select aria-label="Default select example">
+            <option>Open this select menu</option>
+            <option value="1">Barcelona</option>
+            <option value="2">Juventus</option>
+            <option value="3">Bayern Munich</option>
+        </Form.Select>
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Guardar
+      </Button>
+    </Form>
   )
 }
 
